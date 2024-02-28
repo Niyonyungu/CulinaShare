e2e2fb682055780b60a385b05889664e8891008e
Michel vainqueur <vainqueurmg@gmail.com>
2024-02-28 11:39:03 +0200

    fix/recipes

--- src/components/pageProps/FavePage/ProductBanner.js
+++ src/components/pageProps/FavePage/ProductBanner.js
@@ -1,51 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { BsGridFill } from "react-icons/bs";
-import { ImList } from "react-icons/im";
+import React from "react";
 import { GoTriangleDown } from "react-icons/go";
 
 const ProductBanner = ({ itemsPerPageFromBanner }) => {
 
-  const [girdViewActive, setGridViewActive] = useState(true);
-  const [listViewActive, setListViewActive] = useState(false);
-  useEffect(() => {
-    const gridView = document.querySelector(".gridView");
-    const listView = document.querySelector(".listView");
-
-    gridView.addEventListener("click", () => {
-      setListViewActive(false);
-      setGridViewActive(true);
-    });
-    listView.addEventListener("click", () => {
-      setGridViewActive(false);
-      setListViewActive(true);
-    });
-  }, [girdViewActive, listViewActive]);
 
   return (
     <div className="w-full flex flex-col md:flex-row md:items-center justify-between">
-      <div className="flex items-center gap-4">
-        <span
-          className={`${
-            girdViewActive
-              ? "bg-[#808000] text-white"
-              : "border-[1px] border-gray-300 text-[#737373]"
-          } w-8 h-8 text-lg flex items-center justify-center cursor-pointer gridView`}
-        >
-          <BsGridFill />
-        </span>
-        <span
-          className={`${
-            listViewActive
-              ? "bg-[#808000] text-white"
-              : "border-[1px] border-gray-300 text-[#737373]"
-          } w-8 h-8 text-base flex items-center justify-center cursor-pointer listView`}
-        >
-          <ImList />
-        </span>
-      </div>
       <div className="flex items-center gap-2 md:gap-6 mt-4 md:mt-0">
-     
-        <div className="flex items-center gap-2 text-[#767676] relative">
+        <div className="flex items-center gap-2 text-[#767676] relative float-right" >
           <label className="block font-black text-black">Show:</label>
           <select
             onChange={(e) => itemsPerPageFromBanner(+e.target.value)}
